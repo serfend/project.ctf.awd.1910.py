124d5cf898c56801324019147096a78ac85e9c1b
汉广 <liuchangm7j@vip.qq.com>
2019-10-14 22:22:23 +0800

    new future defender depoly

--- src/Defends/Defender.py
+++ src/Defends/Defender.py
@@ -5,9 +5,12 @@ class defender:
     def __init__(self,ssh,config):
         self.ssh=ssh
         self.killer=config["env"]["troj"]["killer"]
+        self.
         pass
     def killTroj(self):
         #使用ssh创建临时马，killall -uwww-data，ssh.deploy(#rawBck#)，删除临时马
         
         self.ssh.execCmd(self.killer)
         ssh.deploy(ssh.rawBck)
+    def deployDefender(self):
+        pass
