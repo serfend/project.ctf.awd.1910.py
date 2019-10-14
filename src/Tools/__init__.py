2404be7b3015bac221f1db0d27cb3bd025825bea
汉广 <liuchangm7j@vip.qq.com>
2019-10-14 18:51:28 +0800

    Update __init__.py

--- src/Tools/__init__.py
+++ src/Tools/__init__.py
@@ -2,4 +2,6 @@
 from Tools.CmdBuilder import *
 from Tools.RangeOperate import *
 from Tools.Runner import *
+from Tools.CmdExec import *
+from Tools.RuntimeSetting import *
 import Tools.BackDoorDescription
