ac2d40f645e1a7cacf99aaa57255809a9f7f35d3
汉广 <liuchangm7j@vip.qq.com>
2019-10-14 18:51:55 +0800

    Update Config.py

--- src/Setting/Config.py
+++ src/Setting/Config.py
@@ -5,24 +5,26 @@ import time
 import json
 import os
 import sys
+# import Celery
+
 class Config:
-    __defaultSettingPath='setting/default.json'
-    
+    defaultSettingPath='setting'
+    root=os.getcwd()
+    exepath=sys.path[0]
     def __init__(self,settingPath=''):
         self.load(settingPath)
-        pass
     def load(self,settingPath=''):
-        
         if settingPath!='':
             self.settingPath=settingPath
         if self.settingPath=='':
             raise Exception("setting path is null")
-        path=f'{sys.path[0]}/{self.settingPath}'
+        path=f'{sys.path[0]}/{Config.defaultSettingPath}/{self.settingPath}'
         print(f'Config.loadSetting:{path}')
-        file=open(path,'r',encoding='utf-8')
-        content=file.read()
-        self.obj=json.loads(content)
-        file.close()
+        with open(path,'r',encoding='utf-8') as file:
+            self.obj=json.load(file)
+        self.autoReload()
+    def autoReload(self):
+        self.interval=self.obj['system']['reloadSettingInterval']
     def __getitem__(self,index):
         return self.obj
     def __setitem__(self,index,v):
