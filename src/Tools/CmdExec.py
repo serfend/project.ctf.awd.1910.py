eb61b23236b151378b3b23b1b058229fc83adac4
汉广 <liuchangm7j@vip.qq.com>
2019-10-17 18:59:29 +0800

    Update CmdExec.py

--- src/Tools/CmdExec.py
+++ src/Tools/CmdExec.py
@@ -1,9 +1,11 @@
 import os
+import Tools.CmdBuilder
 class CmdExec:
     def execCmd(self,cmd):
         print(f'os.system:{cmd}')
         return (os.system(cmd))
 class CC:
+    DisplayRank=0
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -15,3 +17,9 @@ class CC:
     @staticmethod
     def r(info,title='\033[95m'):
         return f'{title}{info}\'\033[0m\''
+    """
+    控制台输出
+    """
+    @staticmethod
+    def i(info,rank=0):
+        if rank>=CC.DisplayRank:print(f'{Tools.CmdBuilder.StandardTime()} {info}')
