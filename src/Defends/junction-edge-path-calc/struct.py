8a1d281f9517fa91592c824d0b70f0f59ae15039
汉广 <serfend@foxmail.com>
2022-03-05 15:49:01 +0800

    feat[user-proj]update flow graph

---
+++ src/Defends/junction-edge-path-calc/struct.py
@@ -0,0 +1,15 @@
+# 道路路段
+class Edge:
+    def __init__(self):
+        pass
+
+# 道路节点
+class JunctionTypePrimary:
+    def __init__(self):
+        pass
+
+
+# 道路交叉路况的左右拐节点
+class JunctionTypeCrossChildren:
+    def __init__(self):
+        pass
